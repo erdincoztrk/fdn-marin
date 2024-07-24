39a2db2e651c9752bf2110a80e61b3be60c102b4
erdincozturk <e.erdincoztrk@gmail.com>
2024-07-25 00:09:29 +0300

    - Mobil kontrol fonksiyonu eklendi. - Logonun mobilde ortada gözükmesi ayarlandı.

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,31 @@
     var year = new Date().getFullYear();
     document.querySelector("#currentYear").innerHTML = year;
 })();
+
+
+
+function hakkimizdaGit(){
+        // Hedef bölüme yumuşak kayma
+        document.getElementById('hakkimizda').scrollIntoView({ behavior: 'smooth' });
+}
+
+function isMobile(){
+    if(window.innerWidth <= 768)
+        return true;
+    else
+        return false;
+}
+
+function logoMove(){
+    const mobileControl = isMobile();
+    if(mobileControl){
+
+        document.getElementsByClassName('navbar-brand')[0].querySelector('img').style.display = 'none';
+
+        document.getElementById('logoDetail').querySelector('h1').style.display = 'none';
+
+        document.getElementById('logo').innerHTML =
+            '<img src="images/fdn-marine-Photoroom-beyaz.png">'
+        ;
+    }
+}
