10f40a8a75cb4a0106660b906dd1359d36aa2056
erdincozturk <e.erdincoztrk@gmail.com>
2024-07-25 00:11:12 +0300

    navbar>navbar.phpye atandı.

---
+++ navbar.php
@@ -0,0 +1,17 @@
+<div class="collapse navbar-collapse ml-auto" style="margin-top:0;" id="navbarSupportedContent">
+    <ul class="navbar-nav  ">
+        <li class="nav-item active">
+            <a class="nav-link" href="index.php">Anasayfa <span
+                    class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="about.html"> Hakkımızda</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="service.html"> Modellerimiz </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="contact.html">İletişim</a>
+        </li>
+    </ul>
+</div>
