b891f8a8c1b2bb2263cdc956ca8072000a20855d
erdincozturk <e.erdincoztrk@gmail.com>
2024-08-23 20:41:54 +0300

    Hakkımda bilgileri güncelleme işlemleri yapıldı.

--- admin/js/app.js
+++ admin/js/app.js
@@ -6,3 +6,12 @@ function setCommunication(form){
         data: $(form).serialize()
     })
 }
+
+function setAboutUs(form){
+    const file = 'db-transactions/aboutus-transactions.php';
+    $.ajax({
+        url: file,
+        type: 'POST',
+        data: $(form).serialize()
+    })
+}
