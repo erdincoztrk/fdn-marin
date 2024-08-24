949994baa6525059a955c043e98bf588ce5f3d99
erdincozturk <e.erdincoztrk@gmail.com>
2024-08-24 15:24:23 +0300

    Hakkımda.php ck editör kaldırıldı.

--- admin/hakkimda.php
+++ admin/hakkimda.php
@@ -1,5 +1,8 @@
 <?php
 include 'header.php';
+include_once('islemler/baglan.php');
+$db = new dbConnection();
+$getAboutUs = $db->getRow("SELECT * FROM tbaboutus");
 ?>
     <div class="container">
         <h4>Hakkımda Bilgi Düzenleme</h4>
@@ -8,20 +11,20 @@ include 'header.php';
             <div class="form-group">
                 <label for="exampleFormControlInput1">Şirket Adı</label>
                 <input type="text" class="form-control" name="siteName"
-                          id="siteName" value="">
+                          id="siteName" value="<?=$getAboutUs['about_siteName']?>">
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Kuruluş Tarihi</label>
                 <input type="date" class="form-control" id="exampleFormControlInput1" name="foundationDate"
-                       id="foundationDate" value="">
+                       id="foundationDate" value="<?=$getAboutUs['about_foundationDate']?>">
             </div>
 
 
             <div class="form-group">
                 <label for="exampleFormControlInput1">Biz Kimiz?</label>
-                <textarea rows="4" cols="40" class="form-control ckeditor" name="aboutUs"
-                          id="aboutUs" value=""></textarea>
+                <textarea rows="4" cols="40" class="form-control" name="aboutUs"
+                          id="aboutUs"><?=$getAboutUs['about_us']?></textarea>
             </div>
 
 
@@ -29,6 +32,7 @@ include 'header.php';
             <button type="button" class="btn btn-success" align="right" onclick="setAboutUs($('#aboutusForm'))">
                 Düzenle
             </button>
+
 
 
         </form>
