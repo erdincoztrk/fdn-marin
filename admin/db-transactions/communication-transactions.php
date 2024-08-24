18c284674ab8bc5ab8d9e52eb695e51a63f5877f
erdincozturk <e.erdincoztrk@gmail.com>
2024-08-24 14:37:40 +0300

    İletişim ayar için mail eklendi. güncelleme işlemleri ayarlandı.

--- admin/db-transactions/communication-transactions.php
+++ admin/db-transactions/communication-transactions.php
@@ -1,6 +1,7 @@
 <?php include('../islemler/baglan.php');
 $db = new dbConnection();
 $tel = $_POST['tel'];
+$mail = $_POST['mail'];
 $instagram = $_POST['instagram'];
 $facebook = $_POST['facebook'];
 $sahibinden = $_POST['sahibinden'];
@@ -9,8 +10,8 @@ $distinct = $_POST['distinct'];
 $adress = $_POST['adress'];
 $googleMap = $_POST['googleMap'];
 
-$commArray = [$tel, $instagram, $facebook, $sahibinden, $city, $distinct, $adress, $googleMap];
+$commArray = [$tel, $instagram, $facebook, $sahibinden, $city, $distinct, $adress, $googleMap, $mail];
 
 $db->query("DELETE FROM tbcommunication");
-$addQuery = $db->query("INSERT INTO tbcommunication (comm_tel, comm_instagram, comm_facebook, comm_sahibinden, comm_city, comm_distinct, comm_adress, comm_googlemap) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", $commArray);
+$addQuery = $db->query("INSERT INTO tbcommunication (comm_tel, comm_instagram, comm_facebook, comm_sahibinden, comm_city, comm_distinct, comm_adress, comm_googlemap, comm_mail) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", $commArray);
 echo $addQuery ? 'successful' : 'unsuccessful';
--- admin/iletisim-ayar.php
+++ admin/iletisim-ayar.php
@@ -1,4 +1,9 @@
-<?php  include 'header.php' ?>
+<?php
+include 'header.php';
+require_once('islemler/baglan.php');
+$db = new dbConnection();
+$getComm = $db->getRow("SELECT * FROM tbcommunication");
+?>
 <div class="container">
 <h4>İLETİŞİM BİLGİLERİ GÜNCELLEME</h4>
 
@@ -7,47 +12,47 @@
 
    <div class="form-group">
     <label for="exampleFormControlInput1">Telefon Numarası</label>
-    <input type="text" class="form-control" name="tel" id="tel" value="" >
+    <input type="text" class="form-control" name="tel" id="tel" value="<?=$getComm['comm_tel']?>" >
   </div>
 
   <div class="form-group">
     <label for="exampleFormControlInput1">Mail Adresi</label>
-    <input type="text" class="form-control" name="mail" id="mail" value="">
+    <input type="text" class="form-control" name="mail" id="mail" value="<?=$getComm['comm_mail']?>">
   </div>
 
   <div class="form-group">
     <label for="exampleFormControlInput1">Instagram</label>
-    <input type="text" class="form-control" name="instagram" id="instagram" value="">
+    <input type="text" class="form-control" name="instagram" id="instagram" value="<?=$getComm['comm_instagram']?>">
   </div>
 
   <div class="form-group">
     <label for="exampleFormControlInput1">Facebook</label>
-    <input type="text" class="form-control" name="facebook" id="facebook" value="">
+    <input type="text" class="form-control" name="facebook" id="facebook" value="<?=$getComm['comm_facebook']?>">
   </div>
 
   <div class="form-group">
     <label for="exampleFormControlInput1">Sahibinden</label>
-    <input type="text" class="form-control" name="sahibinden" id="sahibinden" value="">
+    <input type="text" class="form-control" name="sahibinden" id="sahibinden" value="<?=$getComm['comm_sahibinden']?>">
   </div>
 
   <div class="form-group">
     <label for="exampleFormControlInput1">İl</label>
-    <input type="text" class="form-control" name="city" id="city" value="">
+    <input type="text" class="form-control" name="city" id="city" value="<?=$getComm['comm_city']?>">
   </div>
 
   <div class="form-group">
     <label for="exampleFormControlInput1">İlçe</label>
-    <input type="text" class="form-control" name="distinct" id="distinct" value="">
+    <input type="text" class="form-control" name="distinct" id="distinct" value="<?=$getComm['comm_distinct']?>">
   </div>
 
   <div class="form-group">
     <label for="exampleFormControlInput1">Adres</label>
-    <input type="text" class="form-control" name="adress"  id="adress" value="">
+    <input type="text" class="form-control" name="adress"  id="adress" value="<?=$getComm['comm_adress']?>">
   </div>
 
   <div class="form-group">
     <label for="exampleFormControlInput1">Google Map</label>
-    <input type="text" class="form-control" name="googleMap" id="googleMap" value="">
+    <input type="text" class="form-control" name="googleMap" id="googleMap" value="<?=$getComm['comm_googlemap']?>">
   </div>
 
  
