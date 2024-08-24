8106eec2fabb19857f2e7d99e6361d8996410591
erdincozturk <e.erdincoztrk@gmail.com>
2024-08-24 15:17:47 +0300

    - Loader js işlemleri ayarlandı. - Datadan dönen mesaj ayarları yapıldı. - SweetConfirm eklendi.

--- admin/islemler/baglan.php
+++ admin/islemler/baglan.php
@@ -1,11 +1,42 @@
-<?php 
+<?php
+class dbConnection{
+	private $host = 'localhost';
+	private $dbName = 'fdn-marin';
+	private $username = 'root';
+	private $password = 'root';
+	private $db = '';
 
-try {
-	$db=new PDO("mysql:host=localhost;dbname=cvsite;charset=utf8",'root','');
-	
-} catch (PDOException $e) {
-	echo $e -> getMessage();
+	public function __construct()
+	{
+		$connectionString = "mysql:host=$this->host;dbname=$this->dbName";
+		try {
+			$this->db = new PDO($connectionString,$this->username,$this->password);
+			$this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		}
+		catch (\PDOException $e){
+			echo "DATABASE CONNECTION WAS FAILED $e->getMessage()";
+		}
+	}
+
+	public function getRow($query){
+		return $this->db->query($query)->fetch(PDO::FETCH_ASSOC);
+	}
+	public function getRows($query){
+		return $this->db->query($query)->fetchAll(PDO::FETCH_ASSOC);
+	}
+	public function query($query,$parameters=null){
+		if($parameters){
+			$result = $this->db->prepare($query)->execute($parameters);
+		}
+		else{
+			$result = $this->db->prepare($query)->execute();
+		}
+		if($result){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 }
-
-
- ?>
+?>
--- admin/js/app.js
+++ admin/js/app.js
@@ -1,17 +1,49 @@
-function setCommunication(form){
+function showLoader() {
+    document.getElementById('loader-container').style.display = 'block';
+}
+
+function hideLoader() {
+    document.getElementById('loader-container').style.display = 'none';
+}
+
+function smallNotice(text, icon, timer = 3000) {
+    Swal.fire({
+        position: 'bottom-end',
+        icon: icon,
+        title: text,
+        showConfirmButton: false,
+        timer: timer,
+        toast: true
+    });
+}
+
+function dataMessage(data) {
+    hideLoader();
+    data == 'successful' ? smallNotice('İşlem Başarılı!', 'success') : smallNotice('İşlem Başarısız', 'error')
+}
+
+function setCommunication(form) {
     const file = 'db-transactions/communication-transactions.php';
+    showLoader();
     $.ajax({
         url: file,
         type: 'POST',
-        data: $(form).serialize()
+        data: $(form).serialize(),
+        success: (data) => {
+            dataMessage(data)
+        }
     })
 }
 
-function setAboutUs(form){
+function setAboutUs(form) {
     const file = 'db-transactions/aboutus-transactions.php';
+    showLoader();
     $.ajax({
         url: file,
         type: 'POST',
-        data: $(form).serialize()
+        data: $(form).serialize(),
+        success: (data) => {
+            dataMessage(data);
+        }
     })
 }
