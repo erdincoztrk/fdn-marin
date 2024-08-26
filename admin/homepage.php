9b2fad73777a9845f4152e135c4fd20fed49537b
erdincozturk <e.erdincoztrk@gmail.com>
2024-08-26 23:43:56 +0300

    homepage.php işlemi tamamlandı.

--- admin/homepage.php
+++ admin/homepage.php
@@ -5,10 +5,10 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Hoşgeldin <?php echo $admin['admin_ad']." ".$admin['admin_soyad'] ?></h1>
+                        <h1 class="h3 mb-0 text-gray-800">Hoşgeldin </h1>
                         
                     </div>
-                    <p>İşlemleri Sol Taraftaki Menüden Seçebilirsin. Admin Profilini Görüntülemek İçin Sağ Üstteki Alana Tıklayınız.</p>
+                    <p>İşlemleri Sol Taraftaki Menüden Seçebilirsin.</p>
 
                 
                     
