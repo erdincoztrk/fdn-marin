c778786072408a8e3e25b1cccf2a16636661ce74
erdincozturk <e.erdincoztrk@gmail.com>
2024-08-30 16:26:27 +0300

    otomatik sitemap oluşturmak için php işlemleri yapıldı. sitemap.xml yazıldı. .htaccesste değişiklikler yapıldı.

---
+++ admin/islemler/sitemap.php
@@ -0,0 +1,50 @@
+<?php
+include 'baglan.php';
+$db = new dbConnection();
+$sitemapPath = '../../sitemap.xml';
+$xml = '
+<?xml version="1.0" encoding="utf-8" standalone="yes" ?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://www.fdnmarine.com/</loc>
+        <lastmod>2024-08-30</lastmod>
+        <changefreq>always</changefreq>
+        <priority>N/A</priority>
+    </url>
+    <url>
+        <loc>https://www.fdnmarine.com/index.php</loc>
+        <lastmod>2024-08-30</lastmod>
+        <changefreq>always</changefreq>
+        <priority>N/A</priority>
+    </url>
+    <url>
+        <loc>https://www.fdnmarine.com/modeller.php</loc>
+        <lastmod>2024-08-30</lastmod>
+        <changefreq>always</changefreq>
+        <priority>N/A</priority>
+    </url>
+    <url>
+        <loc>https://www.fdnmarine.com/iletisim.php</loc>
+        <lastmod>2024-08-30</lastmod>
+        <changefreq>always</changefreq>
+        <priority>N/A</priority>
+    </url>
+';
+$getmodels = $db->getRows("SELECT * FROM tbproduct");
+$url = 'https://www.fdnmarine.com/model-detay.php?';
+foreach ($getmodels as $product){
+    $url .= 'model='.$product['product_url'].'&amp;mid?='.$product['product_id'];
+    $xml .= '
+        <url>
+        <loc>'.$url.'</loc>
+        <lastmod>'.date("Y/m/d").'</lastmod>
+        <changefreq>always</changefreq>
+        <priority>N/A</priority>
+    </url>
+    ';
+}
+$xml .= '</urlset>';
+
+file_put_contents($sitemapPath, $xml);
+
+echo 'Sitemap başarıyla güncellendi!';
